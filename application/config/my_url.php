0c1394e76098caf57b1e2d8a40438ba9e0a5472d
zengpan <zengpan@ymt360.com>
2015-06-03 11:40:50 +0800

    first

---
+++ application/config/my_url.php
@@ -0,0 +1,30 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//$config['www_site_url'] = 'http://www.ymt.com';
+//$config['bbs_url']      = 'http://bbs.ymt.com';
+//$config['pic_url']      = 'http://pic.ymt.com';
+//$config['wap_url']      = 'http://m.ymt.com';
+//$config['ji_url']       = 'http://ji.ymt.com';
+//$config['app_url']      = 'http://app.ymt.com';
+//$config['wuliu_url']    = 'http://wuliu.ymt.com';
+//$config['jifen_url']    = 'http://jifen.ymt.com';
+//$config['hangqing_url'] = 'http://hangqing.ymt.com';
+//$config['shennong_url'] = 'http://shennong.ymt.com';
+//$config['ye_url']       = 'http://ye.ymt.com';
+//$config['quan_url']     = 'http://quan.ymt.com';
+//$config['news_url']     = 'http://zixun.ymt.com';
+//$config['shuju_url']    = 'http://shuju.ymt.com';
+//$config['pay_url']      = 'https://pay.ymt360.com';
+//$config['shangji_url']  = 'http://shangji.ymt.com';
+
+//$config['ios_release_url']  = 'https://app.ymt.com/ios/';
+//
+//$config['img_url']          = 'http://img.yimutian.com';
+//$config['video_url']        = 'http://video.yimutian.com';
+
+$config['static_url']       = array('http://s.tests.cn'  => 1);
+
+//$config['shop_host']        = 's.ymt.com';
+//
+//$config['img_upload_url']   = 'http://upload.ymt.com/img';
+//$config['video_upload_url'] = 'http://upload.ymt.com/video';
---
+++ shared/helpers/my_url_helper.php
@@ -0,0 +1,28 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ *自定义helper文件
+ */
+if ( ! function_exists('static_url')) { 
+    function static_url($uri = '', $timestamp = '') {
+        static $s_base_url;
+        if( ! $s_base_url) {
+            $ci =& get_instance();
+            $ci->load->config('my_url');
+            $static_arr = $ci->config->item('static_url');
+            $s_base_url = '';
+            $static_sum = array_sum($static_arr);
+            foreach ($static_arr as $url => $static_num) {
+                $rand_num = mt_rand(1, $static_sum);
+                if ($rand_num <= $static_num) {
+                    $s_base_url = rtrim($url, '/') . '/';
+                    break;
+                } else {
+                    $static_sum -= $static_num;
+                }
+            }
+        }
+        return $s_base_url . $uri . ( ! empty($timestamp) ? "?$timestamp" : '');
+    }
+}
+
