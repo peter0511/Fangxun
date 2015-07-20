e8f86fdb69783603a8471b9556b1a172b5286cd1
zengpan <zengpan@ymt360.com>
2015-07-20 16:20:03 +0800

    修改部分控制器

---
+++ application/logs/log-2015-07-20.php
@@ -0,0 +1,179 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2015-07-20 16:18:52 --> Config Class Initialized
+INFO - 2015-07-20 16:18:52 --> Hooks Class Initialized
+DEBUG - 2015-07-20 16:18:52 --> UTF-8 Support Enabled
+INFO - 2015-07-20 16:18:52 --> Utf8 Class Initialized
+INFO - 2015-07-20 16:18:52 --> URI Class Initialized
+INFO - 2015-07-20 16:18:52 --> Router Class Initialized
+INFO - 2015-07-20 16:18:52 --> Output Class Initialized
+INFO - 2015-07-20 16:18:52 --> Security Class Initialized
+DEBUG - 2015-07-20 16:18:52 --> Global POST, GET and COOKIE data sanitized
+INFO - 2015-07-20 16:18:52 --> Input Class Initialized
+INFO - 2015-07-20 16:18:52 --> Language Class Initialized
+INFO - 2015-07-20 16:18:52 --> Loader Class Initialized
+INFO - 2015-07-20 16:18:52 --> Helper loaded: url_helper
+INFO - 2015-07-20 16:18:52 --> Helper loaded: my_url_helper
+INFO - 2015-07-20 16:18:52 --> Database Driver Class Initialized
+INFO - 2015-07-20 16:18:52 --> Session: Class initialized using 'database' driver.
+INFO - 2015-07-20 16:18:52 --> Model Class Initialized
+INFO - 2015-07-20 16:18:52 --> Model Class Initialized
+INFO - 2015-07-20 16:18:52 --> Controller Class Initialized
+DEBUG - 2015-07-20 16:18:52 --> Config file loaded: /Users/Peter/Work/test/Fangxun/application/config/my_url.php
+INFO - 2015-07-20 16:18:52 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/top.php
+INFO - 2015-07-20 16:18:52 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/left.php
+INFO - 2015-07-20 16:18:52 --> Model Class Initialized
+DEBUG - 2015-07-20 16:18:52 --> Auth class already loaded. Second attempt ignored.
+INFO - 2015-07-20 16:18:52 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2015-07-20 16:18:52 --> Pagination Class Initialized
+DEBUG - 2015-07-20 16:18:52 --> Config file loaded: /Users/Peter/Work/test/Fangxun/system/../shared/config/user.php
+INFO - 2015-07-20 16:18:53 --> Helper loaded: form_helper
+INFO - 2015-07-20 16:18:53 --> Form Validation Class Initialized
+INFO - 2015-07-20 16:18:53 --> Encrypt Class Initialized
+INFO - 2015-07-20 16:18:53 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/House/address.php
+INFO - 2015-07-20 16:18:53 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/footer.php
+INFO - 2015-07-20 16:18:53 --> Final output sent to browser
+DEBUG - 2015-07-20 16:18:53 --> Total execution time: 0.3486
+INFO - 2015-07-20 16:18:57 --> Config Class Initialized
+INFO - 2015-07-20 16:18:57 --> Hooks Class Initialized
+DEBUG - 2015-07-20 16:18:57 --> UTF-8 Support Enabled
+INFO - 2015-07-20 16:18:57 --> Utf8 Class Initialized
+INFO - 2015-07-20 16:18:57 --> URI Class Initialized
+INFO - 2015-07-20 16:18:57 --> Router Class Initialized
+INFO - 2015-07-20 16:18:57 --> Output Class Initialized
+INFO - 2015-07-20 16:18:57 --> Security Class Initialized
+DEBUG - 2015-07-20 16:18:57 --> Global POST, GET and COOKIE data sanitized
+INFO - 2015-07-20 16:18:57 --> Input Class Initialized
+INFO - 2015-07-20 16:18:57 --> Language Class Initialized
+INFO - 2015-07-20 16:18:57 --> Loader Class Initialized
+INFO - 2015-07-20 16:18:57 --> Helper loaded: url_helper
+INFO - 2015-07-20 16:18:57 --> Helper loaded: my_url_helper
+INFO - 2015-07-20 16:18:57 --> Database Driver Class Initialized
+INFO - 2015-07-20 16:18:57 --> Session: Class initialized using 'database' driver.
+INFO - 2015-07-20 16:18:57 --> Model Class Initialized
+INFO - 2015-07-20 16:18:57 --> Model Class Initialized
+INFO - 2015-07-20 16:18:57 --> Controller Class Initialized
+DEBUG - 2015-07-20 16:18:57 --> Config file loaded: /Users/Peter/Work/test/Fangxun/application/config/my_url.php
+INFO - 2015-07-20 16:18:57 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/top.php
+INFO - 2015-07-20 16:18:57 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/left.php
+INFO - 2015-07-20 16:18:57 --> Model Class Initialized
+DEBUG - 2015-07-20 16:18:57 --> Auth class already loaded. Second attempt ignored.
+INFO - 2015-07-20 16:18:57 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2015-07-20 16:18:57 --> Pagination Class Initialized
+DEBUG - 2015-07-20 16:18:57 --> Config file loaded: /Users/Peter/Work/test/Fangxun/system/../shared/config/user.php
+INFO - 2015-07-20 16:19:17 --> Config Class Initialized
+INFO - 2015-07-20 16:19:17 --> Hooks Class Initialized
+DEBUG - 2015-07-20 16:19:17 --> UTF-8 Support Enabled
+INFO - 2015-07-20 16:19:17 --> Utf8 Class Initialized
+INFO - 2015-07-20 16:19:17 --> URI Class Initialized
+INFO - 2015-07-20 16:19:17 --> Router Class Initialized
+INFO - 2015-07-20 16:19:17 --> Output Class Initialized
+INFO - 2015-07-20 16:19:17 --> Security Class Initialized
+DEBUG - 2015-07-20 16:19:17 --> Global POST, GET and COOKIE data sanitized
+INFO - 2015-07-20 16:19:17 --> Input Class Initialized
+INFO - 2015-07-20 16:19:17 --> Language Class Initialized
+INFO - 2015-07-20 16:19:17 --> Loader Class Initialized
+INFO - 2015-07-20 16:19:17 --> Helper loaded: url_helper
+INFO - 2015-07-20 16:19:17 --> Helper loaded: my_url_helper
+INFO - 2015-07-20 16:19:17 --> Database Driver Class Initialized
+INFO - 2015-07-20 16:19:17 --> Session: Class initialized using 'database' driver.
+INFO - 2015-07-20 16:19:17 --> Model Class Initialized
+INFO - 2015-07-20 16:19:17 --> Model Class Initialized
+INFO - 2015-07-20 16:19:17 --> Controller Class Initialized
+DEBUG - 2015-07-20 16:19:17 --> Config file loaded: /Users/Peter/Work/test/Fangxun/application/config/my_url.php
+INFO - 2015-07-20 16:19:17 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/top.php
+INFO - 2015-07-20 16:19:17 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/left.php
+INFO - 2015-07-20 16:19:17 --> Model Class Initialized
+DEBUG - 2015-07-20 16:19:17 --> Auth class already loaded. Second attempt ignored.
+INFO - 2015-07-20 16:19:17 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2015-07-20 16:19:17 --> Pagination Class Initialized
+DEBUG - 2015-07-20 16:19:17 --> Config file loaded: /Users/Peter/Work/test/Fangxun/system/../shared/config/user.php
+INFO - 2015-07-20 16:19:17 --> Helper loaded: form_helper
+INFO - 2015-07-20 16:19:17 --> Form Validation Class Initialized
+INFO - 2015-07-20 16:19:17 --> Encrypt Class Initialized
+INFO - 2015-07-20 16:19:19 --> Config Class Initialized
+INFO - 2015-07-20 16:19:19 --> Hooks Class Initialized
+DEBUG - 2015-07-20 16:19:19 --> UTF-8 Support Enabled
+INFO - 2015-07-20 16:19:19 --> Utf8 Class Initialized
+INFO - 2015-07-20 16:19:19 --> URI Class Initialized
+INFO - 2015-07-20 16:19:19 --> Router Class Initialized
+INFO - 2015-07-20 16:19:19 --> Output Class Initialized
+INFO - 2015-07-20 16:19:19 --> Security Class Initialized
+DEBUG - 2015-07-20 16:19:19 --> Global POST, GET and COOKIE data sanitized
+INFO - 2015-07-20 16:19:19 --> Input Class Initialized
+INFO - 2015-07-20 16:19:19 --> Language Class Initialized
+INFO - 2015-07-20 16:19:19 --> Loader Class Initialized
+INFO - 2015-07-20 16:19:19 --> Helper loaded: url_helper
+INFO - 2015-07-20 16:19:19 --> Helper loaded: my_url_helper
+INFO - 2015-07-20 16:19:19 --> Database Driver Class Initialized
+INFO - 2015-07-20 16:19:19 --> Session: Class initialized using 'database' driver.
+INFO - 2015-07-20 16:19:19 --> Model Class Initialized
+INFO - 2015-07-20 16:19:19 --> Model Class Initialized
+INFO - 2015-07-20 16:19:19 --> Controller Class Initialized
+DEBUG - 2015-07-20 16:19:19 --> Config file loaded: /Users/Peter/Work/test/Fangxun/application/config/my_url.php
+INFO - 2015-07-20 16:19:19 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/top.php
+INFO - 2015-07-20 16:19:19 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/left.php
+INFO - 2015-07-20 16:19:19 --> Model Class Initialized
+DEBUG - 2015-07-20 16:19:19 --> Auth class already loaded. Second attempt ignored.
+INFO - 2015-07-20 16:19:19 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2015-07-20 16:19:19 --> Pagination Class Initialized
+DEBUG - 2015-07-20 16:19:19 --> Config file loaded: /Users/Peter/Work/test/Fangxun/system/../shared/config/user.php
+DEBUG - 2015-07-20 16:19:19 --> Config file loaded: /Users/Peter/Work/test/Fangxun/system/../shared/config/house.php
+INFO - 2015-07-20 16:19:19 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/House/create.php
+INFO - 2015-07-20 16:19:19 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/footer.php
+INFO - 2015-07-20 16:19:19 --> Final output sent to browser
+DEBUG - 2015-07-20 16:19:19 --> Total execution time: 0.0517
+INFO - 2015-07-20 16:19:26 --> Config Class Initialized
+INFO - 2015-07-20 16:19:26 --> Hooks Class Initialized
+DEBUG - 2015-07-20 16:19:26 --> UTF-8 Support Enabled
+INFO - 2015-07-20 16:19:26 --> Utf8 Class Initialized
+INFO - 2015-07-20 16:19:26 --> URI Class Initialized
+INFO - 2015-07-20 16:19:26 --> Router Class Initialized
+INFO - 2015-07-20 16:19:26 --> Output Class Initialized
+INFO - 2015-07-20 16:19:26 --> Security Class Initialized
+DEBUG - 2015-07-20 16:19:26 --> Global POST, GET and COOKIE data sanitized
+INFO - 2015-07-20 16:19:26 --> Input Class Initialized
+INFO - 2015-07-20 16:19:26 --> Language Class Initialized
+INFO - 2015-07-20 16:19:26 --> Loader Class Initialized
+INFO - 2015-07-20 16:19:26 --> Helper loaded: url_helper
+INFO - 2015-07-20 16:19:26 --> Helper loaded: my_url_helper
+INFO - 2015-07-20 16:19:26 --> Database Driver Class Initialized
+INFO - 2015-07-20 16:19:26 --> Session: Class initialized using 'database' driver.
+INFO - 2015-07-20 16:19:26 --> Model Class Initialized
+INFO - 2015-07-20 16:19:26 --> Model Class Initialized
+INFO - 2015-07-20 16:19:26 --> Controller Class Initialized
+DEBUG - 2015-07-20 16:19:26 --> Config file loaded: /Users/Peter/Work/test/Fangxun/application/config/my_url.php
+INFO - 2015-07-20 16:19:26 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/top.php
+INFO - 2015-07-20 16:19:26 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/left.php
+INFO - 2015-07-20 16:19:26 --> Model Class Initialized
+DEBUG - 2015-07-20 16:19:26 --> Auth class already loaded. Second attempt ignored.
+INFO - 2015-07-20 16:19:26 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2015-07-20 16:19:26 --> Pagination Class Initialized
+DEBUG - 2015-07-20 16:19:26 --> Config file loaded: /Users/Peter/Work/test/Fangxun/system/../shared/config/user.php
+INFO - 2015-07-20 16:19:29 --> Config Class Initialized
+INFO - 2015-07-20 16:19:29 --> Hooks Class Initialized
+DEBUG - 2015-07-20 16:19:29 --> UTF-8 Support Enabled
+INFO - 2015-07-20 16:19:29 --> Utf8 Class Initialized
+INFO - 2015-07-20 16:19:29 --> URI Class Initialized
+INFO - 2015-07-20 16:19:29 --> Router Class Initialized
+INFO - 2015-07-20 16:19:29 --> Output Class Initialized
+INFO - 2015-07-20 16:19:29 --> Security Class Initialized
+DEBUG - 2015-07-20 16:19:29 --> Global POST, GET and COOKIE data sanitized
+INFO - 2015-07-20 16:19:29 --> Input Class Initialized
+INFO - 2015-07-20 16:19:29 --> Language Class Initialized
+INFO - 2015-07-20 16:19:29 --> Loader Class Initialized
+INFO - 2015-07-20 16:19:29 --> Helper loaded: url_helper
+INFO - 2015-07-20 16:19:29 --> Helper loaded: my_url_helper
+INFO - 2015-07-20 16:19:29 --> Database Driver Class Initialized
+INFO - 2015-07-20 16:19:29 --> Session: Class initialized using 'database' driver.
+INFO - 2015-07-20 16:19:29 --> Model Class Initialized
+INFO - 2015-07-20 16:19:29 --> Model Class Initialized
+INFO - 2015-07-20 16:19:29 --> Controller Class Initialized
+DEBUG - 2015-07-20 16:19:29 --> Config file loaded: /Users/Peter/Work/test/Fangxun/application/config/my_url.php
+INFO - 2015-07-20 16:19:29 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/top.php
+INFO - 2015-07-20 16:19:29 --> File loaded: /Users/Peter/Work/test/Fangxun/application/views/Common/left.php
+INFO - 2015-07-20 16:19:29 --> Model Class Initialized
+DEBUG - 2015-07-20 16:19:29 --> Auth class already loaded. Second attempt ignored.
+INFO - 2015-07-20 16:19:29 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2015-07-20 16:19:29 --> Pagination Class Initialized
+DEBUG - 2015-07-20 16:19:29 --> Config file loaded: /Users/Peter/Work/test/Fangxun/system/../shared/config/user.php
